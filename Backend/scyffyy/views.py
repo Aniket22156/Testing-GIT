3c652b924bee581f2e110d18f5a206b29390f6d9
Aniket Sharma <69108762+Aniket22156@users.noreply.github.com>
2023-02-06 11:47:09 +0530

    With User Input
    This is the working model with user input feature

--- Backend/scyffyy/views.py
+++ Backend/scyffyy/views.py
@@ -10,19 +10,23 @@ def code(request):
     #return render(request, "Hello.html")
     #return HttpResponse(code2())
 
-    response = urllib.request.urlopen('https://www.gandhinagaruni.ac.in')
+    url = request.POST.get("url")
+    
+    render(request, "Hello.html")
+    print(type(url))
+
+    response = urllib.request.urlopen(url)
     soup = BeautifulSoup(response, 'html.parser', from_encoding=response.info().get_param('charset'))
-    #return HttpResponse(soup)
+         #return HttpResponse(soup)
 
     if soup.findAll("meta", {"name":"description"}):
         output = (soup.find("meta", attrs={"name":"description"}).get('content'))
-        return render(request, "Hello.html", {"output": output})
+        return render(request, "output.html", {"output": output})
     else:
         print("Error")
+        
 
     # if soup.findAll("title"):
     #     return HttpResponse(soup.find("title").string)
     # else:
     #     print("Error")
-
-    
